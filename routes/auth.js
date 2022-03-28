3232472885d803ef2c9788c12d426c6a06906f32
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-28 10:23:27 +0900

    Fix register username available logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,26 @@
 import { Router } from 'express';
-import passport from 'passport';
 import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
+import { jwtAuth } from '../config/passport.js';
 import { config } from 'dotenv';
 config();
 
 const router = Router();
-// passport.authenticate('local'),
 
 router.post('/register', (req, res) => {
-  console.log('received post login request');
-  User.exists({ username: req.body.username }, (err, exists) => {
+  User.findOne({ username: req.body.username }).exec((err, user) => {
     if (err) {
       res.status = 400;
       res.send('Database error');
     }
-    if (exists) {
+    if (user) {
       res.status = 400;
       res.send('Username is taken.');
     } else {
       bcrypt.hash(req.body.password, bcrypt.genSaltSync()).then((hash) => {
         const user = new User({
-          username: 'test',
+          username: req.body.username,
           hash,
         });
         user.save((err, user) => {
@@ -51,7 +49,6 @@ router.post('/login', (req, res) => {
     if (!user) {
       res.send('username does not exist');
     }
-    console.log('found user: ', user);
     bcrypt.compare(req.body.password, user.hash).then((match) => {
       if (match) {
         const payload = {
@@ -65,12 +62,8 @@ router.post('/login', (req, res) => {
   });
 });
 
-router.get(
-  '/protected',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.send('successfully authenticated');
-  }
-);
+router.get('/protected', jwtAuth, (req, res) => {
+  res.send('successfully authenticated');
+});
 
 export default router;
