1034cd14590ae79f9aad68d7c94be17e60f1e423
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-30 06:41:48 +0900

    Refactor out to postController

---
+++ controllers/postController.js
@@ -0,0 +1,90 @@
+import Post from '../models/Post.js';
+import Comment from '../models/Comment.js';
+
+const getPosts = (req, res) => {
+  Post.find({ published: true })
+    .limit(20)
+    .populate('comments')
+    .exec((err, posts) => {
+      if (err) {
+        res.send(err.message);
+      }
+      res.send(posts);
+    });
+};
+
+const updatePost = (req, res, next) => {
+  Post.findById(req.params.id).exec((err, post) => {
+    if (err) {
+      res.json({ message: err.message });
+    }
+    post.title = req.body.title || post.title;
+    post.body = req.body.body || post.body;
+    post.published = req.body.published || post.published;
+    post.save((err, post) => {
+      if (err) {
+        res.json({ message: err.message });
+      }
+      res.json({ body: req.body, post });
+    });
+  });
+};
+
+const getOnePost = (req, res) => {
+  Post.findById(req.params.id, (err, post) => {
+    if (err) {
+      res.send(err);
+    }
+    if (!post) {
+      res.send('This post does not exist');
+    }
+    res.send(post);
+  });
+};
+
+const createPost = (req, res) => {
+  const post = new Post({
+    creator: req.user.id,
+    title: req.body.title,
+    body: req.body.body,
+  });
+
+  post.save((err, post) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(post);
+  });
+};
+
+const deletePost = (req, res) => {
+  Post.findByIdAndDelete(req.params.id, (err, post) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(post);
+  });
+};
+
+const postComment = (req, res) => {
+  const comment = new Comment({
+    post: req.params.id,
+    name: req.body.name,
+    comment: req.body.comment,
+  });
+  comment.save((err, comment) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(comment);
+  });
+};
+
+export default {
+  postComment,
+  getOnePost,
+  getPosts,
+  updatePost,
+  createPost,
+  deletePost,
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,77 +1,35 @@
 import { Router } from 'express';
 import { jwtAuth } from '../config/passport.js';
-import Post from '../models/Post.js';
-import Comment from '../models/Comment.js';
+import postController from '../controllers/postController.js';
+import {
+  checkValidationResult,
+  postUpdateValidation,
+} from '../config/validationSchemas.js';
 
 const router = Router();
 
 // root is /posts/
 
-// authenticated route that allows viewing of unpublished posts
-router.get('/', jwtAuth, (req, res) => {
-  Post.find()
-    .limit(20)
-    .populate('comments')
-    .exec((err, posts) => {
-      if (err) {
-        res.send(err);
-      }
-      res.send(posts);
-    });
-});
+router.get('/', postController.getPosts);
 
 // get one post by id
-router.get('/:id', (req, res) => {
-  Post.findById(req.params.id, (err, post) => {
-    if (err) {
-      res.send(err);
-    }
-    if (!post) {
-      res.send('This post does not exist');
-    }
-    res.send(post);
-  });
-});
+router.get('/:id', postController.getOnePost);
 
 // create a post
-router.post('/', jwtAuth, (req, res) => {
-  const post = new Post({
-    creator: req.user.id,
-    title: req.body.title,
-    body: req.body.body,
-  });
-
-  post.save((err, post) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(post);
-  });
-});
+router.post('/', jwtAuth, postController.createPost);
 
 // DELETE a post by id
-router.delete('/:id', jwtAuth, (req, res) => {
-  Post.findByIdAndDelete(req.params.id, (err, post) => {
-    if (err) {
-      res.send(err);
-    }
-    res.send(post);
-  });
-});
+router.delete('/:id', jwtAuth, postController.deletePost);
 
-// POST
-router.post('/:id/comments', (req, res) => {
-  const comment = new Comment({
-    post: req.params.id,
-    name: req.body.name,
-    comment: req.body.comment,
-  });
-  comment.save((err, comment) => {
-    if (err) {
-      res.send(err);
-    }
-    res.send(comment);
-  });
-});
+// POST a comment
+router.post('/:id/comments', postController.postComment);
+
+// UPDATE a post
+router.put(
+  '/:id',
+  postUpdateValidation,
+  checkValidationResult,
+  postController.updatePost
+);
 
 export default router;
