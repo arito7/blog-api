874bd9eb94a05c7112eb4eb3fced0a40f461145b
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-30 06:40:50 +0900

    Add error handler

--- app.js
+++ app.js
@@ -13,10 +13,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', authRoute);
-app.get('/', (req, res) => {
-  res.send('test');
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(
+    JSON.stringify({ error: { message: err.message || 'Serverside Error' } })
+  );
+});
+
+app.listen(5000, () => {
+  console.log('listening on port 5000');
 });
