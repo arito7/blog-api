b2837ea32cfa8b9934ff78c6478b828657aba11a
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-30 06:45:18 +0900

    Use checkvalidation middleware

--- routes/auth.js
+++ routes/auth.js
@@ -4,59 +4,54 @@ import User from '../models/User.js';
 import jwt from 'jsonwebtoken';
 import { jwtAuth } from '../config/passport.js';
 import {
+  checkValidationResult,
   signupValidation,
   loginValidation,
 } from '../config/validationSchemas.js';
-import { validationResult } from 'express-validator';
 import { config } from 'dotenv';
 config();
 
 const router = Router();
 
-router.post('/register', signupValidation, (req, res) => {
-  const valErrors = validationResult(req);
-  if (!valErrors.isEmpty()) {
-    res.status = 400;
-    return res.json({ error: valErrors.array() });
+router.post(
+  '/register',
+  signupValidation,
+  checkValidationResult,
+  (req, res) => {
+    User.findOne({ username: req.body.username }).exec((err, user) => {
+      if (err) {
+        res.status = 400;
+        res.send('Database error');
+      }
+      if (user) {
+        res.status = 400;
+        res.json({ message: 'Username is taken.' });
+      } else {
+        bcrypt.hash(req.body.password, bcrypt.genSaltSync()).then((hash) => {
+          const user = new User({
+            username: req.body.username,
+            hash,
+          });
+          user.save((err, user) => {
+            if (err) {
+              res.send(err);
+            }
+            const _id = user.id;
+            const expiresIn = '14d';
+            const payload = {
+              sub: _id,
+              iat: Date.now(),
+            };
+            const token = jwt.sign(payload, process.env.JWT_SECRET);
+            res.json({ success: true, token, expiresIn });
+          });
+        });
+      }
+    });
   }
-  User.findOne({ username: req.body.username }).exec((err, user) => {
-    if (err) {
-      res.status = 400;
-      res.send('Database error');
-    }
-    if (user) {
-      res.status = 400;
-      res.json({ message: 'Username is taken.' });
-    } else {
-      bcrypt.hash(req.body.password, bcrypt.genSaltSync()).then((hash) => {
-        const user = new User({
-          username: req.body.username,
-          hash,
-        });
-        user.save((err, user) => {
-          if (err) {
-            res.send(err);
-          }
-          const _id = user.id;
-          const expiresIn = '14d';
-          const payload = {
-            sub: _id,
-            iat: Date.now(),
-          };
-          const token = jwt.sign(payload, process.env.JWT_SECRET);
-          res.json({ success: true, token, expiresIn });
-        });
-      });
-    }
-  });
-});
+);
 
-router.post('/login', loginValidation, (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(403);
-    res.json({ errors: errors.array() });
-  }
+router.post('/login', loginValidation, checkValidationResult, (req, res) => {
   User.findOne({ username: req.body.username }, (err, user) => {
     if (err) {
       res.json({ message: err.message });
