d4c2e090acbb1fff854e1d7a8669fe79d8eea47f
arito7 <64824933+arito7@users.noreply.github.com>
2022-03-30 06:41:21 +0900

    Add validation schemas

---
+++ config/validationSchemas.js
@@ -0,0 +1,42 @@
+import { body, validationResult } from 'express-validator';
+
+export const checkValidationResult = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json(errors.array());
+  }
+  return next();
+};
+
+export const signupValidation = [
+  body('username', 'Username field is required')
+    .exists()
+    .trim()
+    .isAlphanumeric()
+    .toLowerCase()
+    .escape(),
+  body('password', 'Password cannot be empty').exists().trim().escape(),
+  body('rpassword')
+    .exists()
+    .custom((val, { req }) => val === req.body.password)
+    .withMessage('Passwords do not match.')
+    .trim()
+    .escape(),
+];
+
+export const loginValidation = [
+  body('username').exists().trim().isAlphanumeric().toLowerCase().escape(),
+  body('password').exists().trim().escape(),
+];
+
+export const postUpdateValidation = [
+  body('title').optional().trim().escape(),
+  body('body').optional().trim().escape(),
+  body('published')
+    .isBoolean()
+    .withMessage('Published must be boolean value.')
+    .optional()
+    .trim()
+    .escape(),
+];
