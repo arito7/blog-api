3397cb8a7abbff65d632c2f272d041894d4926c7
Yuji T <accmanager1990@gmail.com>
2022-03-30 13:46:45 +0900

    Add post create validation

--- config/validationSchemas.js
+++ config/validationSchemas.js
@@ -4,7 +4,7 @@ export const checkValidationResult = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.status(400);
-    res.json(errors.array());
+    return res.json(errors.array());
   }
   return next();
 };
@@ -39,4 +39,15 @@ export const postUpdateValidation = [
     .optional()
     .trim()
     .escape(),
+];
+
+export const postCreateValidation = [
+  body('title').exists().trim().escape(),
+  body('body').exists().trim().escape(),
+  body('published')
+    .optional()
+    .trim()
+    .escape()
+    .isBoolean()
+    .withMessage('Published must be boolean value.'),
 ];
