14aad7405dff610877833d60b99964c8f9c6f45d
Yuji T <accmanager1990@gmail.com>
2022-03-30 13:47:49 +0900

    Add helmet and compression

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import postsRoute from './routes/posts.js';
 import cors from 'cors';
 import morgan from 'morgan';
 import './config/passport.js';
+import compression from 'compression';
+import helmet from 'helmet';
 
 db.on('error', () => {
   console.log('mongodb connection error');
@@ -13,9 +15,12 @@ db.on('error', () => {
 const app = express();
 app.use(
   cors({
+    allowedHeaders: '*',
     origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
   })
 );
+app.use(helmet());
+app.use(compression());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
